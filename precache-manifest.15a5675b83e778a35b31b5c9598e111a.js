47d79ca68e8983c6af085d567be5c911546e3436
Simonka <simonka@pop-os.localdomain>
2019-12-24 15:05:26 +0800

    Updates

---
+++ precache-manifest.15a5675b83e778a35b31b5c9598e111a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a357cfc4c7b7a2a9d9dd86dbb8dd5b2",
+    "url": "/test-site/index.html"
+  },
+  {
+    "revision": "58acc89184384d25718b",
+    "url": "/test-site/static/css/main.e211f83b.chunk.css"
+  },
+  {
+    "revision": "4e8fc3efdae889f5c0ef",
+    "url": "/test-site/static/js/2.fa4a86c4.chunk.js"
+  },
+  {
+    "revision": "9d1cdfe2884807ed781923a7c9100f10",
+    "url": "/test-site/static/js/2.fa4a86c4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "58acc89184384d25718b",
+    "url": "/test-site/static/js/main.059bdabc.chunk.js"
+  },
+  {
+    "revision": "45699d9c21bb58dea65e",
+    "url": "/test-site/static/js/runtime-main.2e6ae07e.js"
+  },
+  {
+    "revision": "5c153461d0b2deeb792d32ed3e711cc9",
+    "url": "/test-site/static/media/cold.5c153461.jpg"
+  },
+  {
+    "revision": "913f2d97a6e1faf0e648757456e5aba9",
+    "url": "/test-site/static/media/day.913f2d97.jpg"
+  },
+  {
+    "revision": "b899ac178eec8eec528ac3244efaf0e6",
+    "url": "/test-site/static/media/warm.b899ac17.jpg"
+  }
+]);
